8767e638f37599a71f32654936a7c46d95edd5b4
Arabella Hooper <arabella.hooper@ecodev.vic.gov.au>
2024-02-09 18:50:59 +1100

    Assignment 2 code

---
+++ Python-Lesson-02/code_like_a_girl_02.py
@@ -0,0 +1,7 @@
+original_text = "I love sunshine, rainbows and lollipops! "
+shouting_text = original_text.upper()
+annoying_text = shouting_text * 10
+num_characters = len(annoying_text )
+
+print(f"My annoying text is {annoying_text}")
+print(f"It has {num_characters} characters.")
