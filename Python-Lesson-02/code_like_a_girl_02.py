6be9233673b013477382c82d87347a220b194702
Arabella Hooper <arabella.hooper@ecodev.vic.gov.au>
2024-02-09 18:55:09 +1100

    Ammended variable name

--- Python-Lesson-02/code_like_a_girl_02.py
+++ Python-Lesson-02/code_like_a_girl_02.py
@@ -1,7 +1,7 @@
 original_text = "I love sunshine, rainbows and lollipops! "
 shouting_text = original_text.upper()
 annoying_text = shouting_text * 10
-num_characters = len(annoying_text )
+number_of_characters = len(annoying_text )
 
 print(f"My annoying text is {annoying_text}")
-print(f"It has {num_characters} characters.")
+print(f"It has {number_of_characters} characters.")
